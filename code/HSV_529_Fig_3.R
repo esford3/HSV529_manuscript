79518709afa2074d7ee315d6468b0374dfbfe339
Emily Ford <esford3@rhino02.fhcrc.org>
2024-03-13 10:28:20 -0700

    update fig3

--- code/HSV_529_Fig_3.R
+++ code/HSV_529_Fig_3.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(tidyr)
 library(readr)
 library(data.table)
+library(stringr)
 
 rm(list=ls())
 
@@ -25,6 +26,7 @@ figure_3b_subset_filename = file.path(repo_loc, 'figures/fig_3b2.pdf')
 figure_3c_filename = file.path(repo_loc, 'figures/fig_3c1.pdf')
 figure_3c_subset_filename = file.path(repo_loc, 'figures/fig_3c2.pdf')
 figure_3d_filename = file.path(repo_loc, 'figures/fig_3d.pdf')
+figure_3e_filename = file.path(repo_loc, 'figures/fig_3e.pdf')
 
 ## PREP
 vax <- read_csv(filename_f3)
@@ -216,6 +218,40 @@ vax$dose3 <- ifelse(vax$a80>0 & vax$a90>0, vax$a90/vax$a80,
 table(vax$dose3)
 
 matrix <- vax %>% 
+  group_by(person) %>%
+  summarise(dose1 = NROW(dose1[dose1>=6]),
+            dose2 = NROW(dose2[dose2>=6]),
+            dose3 = NROW(dose3[dose3>=6 & !is.na(dose3)]))
+
+m_long <- matrix %>% select(person, dose1, dose2, dose3) %>%
+  pivot_longer(cols = c(2:4)) %>%
+  mutate(xpos = name) %>%
+  mutate(xpos = factor(xpos, levels = c("dose1", "dose2", "dose3")))
+
+my_comparisons <- list(c("dose1", "dose2"), c("dose2", "dose3"), c("dose1", "dose3"))
+
+a <- ggplot(m_long, aes(x = xpos, y = value, fill = factor(person), group = person)) +
+  geom_bar(stat = "identity", show.legend = T, na.rm = T) +
+  theme_bw() + ggtitle("Expanding clonotypes by dose") +
+  scale_x_discrete(labels = c("dose1" = "Dose 1",
+                              "dose2" = "Dose 2",
+                              "dose3" = "Dose 3")) + 
+  labs(y="Unique clonotypes expanding >6", x= "") +
+  theme(plot.title = element_text(size = 12, hjust = 0.5)) +
+  theme_classic() + 
+  scale_fill_manual(values = person_color, name = "Person", 
+                    labels = c("1", "2", "3", "4", "5", "6", "7", "8", "9")) + 
+  ggpubr::stat_compare_means(comparisons = my_comparisons, paired = TRUE, na.rm = FALSE, 
+                             method = "wilcox", size = 4, label = "p.signif", label.y = c(550, 500, 590)) +
+  ggpubr::stat_compare_means(method = "wilcox", size = 4, label = "p.signif", 
+                             col = "black", hide.ns = TRUE)
+
+pdf(figure_3d_filename, width = 3.5, height = 5)
+a 
+dev.off()
+
+## Figure 3e
+matrix <- vax %>% 
   group_by(person, prev) %>%
   summarise(dose1 = NROW(dose1[dose1>=6]),
             dose2 = NROW(dose2[dose2>=6]),
@@ -228,6 +264,7 @@ m_long <- matrix %>%
   mutate(xpos = factor(xpos, levels = c("prev_dose1", "elic_dose1", 
                                         "prev_dose2", "elic_dose2", 
                                         "prev_dose3", "elic_dose3")))
+my_comparisons <- list(c("prev_dose1", "elic_dose1"), c("prev_dose2", "elic_dose2"), c("prev_dose3", "elic_dose3"))
 
 a <- ggplot(m_long, aes(x = xpos, y = value, fill = factor(person), group = person)) +
   geom_bar(stat = "identity", show.legend = T, na.rm = T) +
@@ -239,7 +276,11 @@ a <- ggplot(m_long, aes(x = xpos, y = value, fill = factor(person), group = pers
                    guide = guide_axis(angle = 45)) + 
   theme_classic() + 
   scale_fill_manual(values = person_color, name = "Person", 
-                    labels = c("1", "2", "3", "4", "5", "6", "7", "8", "9"))
-pdf(figure_3d_filename, width = 5, height = 5)
+                    labels = c("1", "2", "3", "4", "5", "6", "7", "8", "9")) + 
+  ggpubr::stat_compare_means(comparisons = my_comparisons, paired = FALSE, na.rm = FALSE, 
+                             method = "wilcox", size = 4, label = "p.signif", label.y = c(275, 275, 275))+
+  ggpubr::stat_compare_means(method = "wilcox", size = 4, label = "p.signif", hide.ns = TRUE, col = "black")
+  
+pdf(figure_3e_filename, width = 5, height = 5)
 a 
 dev.off()
